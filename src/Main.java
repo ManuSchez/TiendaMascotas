62c178f455d077f7a0e6786da2e2cf084cc20f17
manuel.sanchez <manuelsschez@gmail.com>
2024-02-06 16:51:46 +0100

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Mascotas mascotas = new Mascotas(perro,gato,pajaro,pez);
+    }
+}
---
+++ src/Mascotas.java
@@ -0,0 +1,13 @@
+public class Mascotas {
+    String perro;
+    String gato;
+    String pajaro;
+    String pez;
+
+public void Mascotas (String perro, String gato, String pajaro, String pez){
+    this.perro=perro;
+    this.gato=gato;
+    this.pajaro=pajaro;
+    this.pez=pez;
+    }
+}
